460408616146ed53f2d2dc017be1736cb52d47eb
Danny Havenith <danny.havenith@mapscape.eu>
2015-06-04 16:31:40 +0200

    Generalize SQLPP_ALIAS_PROVIDER so that it can be used in other contexts

--- include/sqlpp11/alias_provider.h
+++ include/sqlpp11/alias_provider.h
@@ -1,17 +1,17 @@
 /*
  * Copyright (c) 2013-2015, Roland Bock
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
- * 
+ *
  *   Redistributions of source code must retain the above copyright notice, this
  *   list of conditions and the following disclaimer.
- * 
+ *
  *   Redistributions in binary form must reproduce the above copyright notice, this
  *   list of conditions and the following disclaimer in the documentation and/or
  *   other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -30,23 +30,33 @@
 #include <type_traits>
 #include <sqlpp11/char_sequence.h>
 
-#define SQLPP_ALIAS_PROVIDER(name) \
-	struct name##_t\
-{\
-	struct _alias_t\
-	{\
-		static constexpr const char _literal[] =  #name;\
-		using _name_t = sqlpp::make_char_sequence<sizeof(_literal), _literal>;\
-		template<typename T>\
-		struct _member_t\
-		{\
-			T name;\
-			T& operator()() { return name; }\
-			const T& operator()() const { return name; }\
-		};\
-	};\
-};\
-constexpr name##_t name = {};
+#define SQLPP_ALIAS_STRUCT_NAMED( name, stringname)                 \
+        struct _alias_t                                             \
+        {                                                           \
+            static constexpr const char _literal[] =  #stringname;  \
+            using _name_t = sqlpp::make_char_sequence<sizeof(_literal), _literal>;\
+            template<typename T>                                    \
+            struct _member_t                                        \
+            {                                                       \
+                T name;                                             \
+                T& operator()() { return name; }                    \
+                const T& operator()() const { return name; }        \
+            };                                                      \
+        }                                                           \
+    /**/
+
+#define SQLPP_ALIAS_STRUCT( name)           \
+    SQLPP_ALIAS_STRUCT_NAMED( name, name)   \
+    /**/
+
+#define SQLPP_ALIAS_PROVIDER( name)     \
+    struct name##_t                     \
+    {                                   \
+            SQLPP_ALIAS_STRUCT( name);  \
+    };                                  \
+    constexpr name##_t name = {};       \
+    /**/
+
 
 namespace sqlpp
 {
